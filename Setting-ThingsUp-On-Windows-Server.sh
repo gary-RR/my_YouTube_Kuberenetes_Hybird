e027ccd27fc2db6137de1bf56afea2b43e903242
gary-RR <gary_rr@yahoo.com>
2021-06-15 16:26:14 -0500

    Add files via upload

---
+++ Setting-ThingsUp-On-Windows-Server.sh
@@ -0,0 +1,87 @@
+#Run this on a Kubernetes node to get cert and token to join a node.
+kubeadm token create --print-join-command
+
+
+#################################################################################################################################
+ssh administrator@10.0.0.191
+#####################################################################Open all ports#############################################
+#Create a new rule and allow all traffic In and out:
+New-NetFireWallRule -DisplayName "Allow All Traffic" -Direction OutBound -Action Allow 
+New-NetFireWallRule -DisplayName "Allow All Traffic" -Direction InBound -Action Allow
+
+#################################################################Install Windows containers and Docker##########################
+Install-WindowsFeature -Name containers
+Restart-Computer -Force
+
+Install-Module DockerMsftProvider -Force
+	
+Install-Package Docker -ProviderName DockerMsftProvider -Force     
+Restart-Computer -Force
+
+Set-Service -Name docker -StartupType 'Automatic'
+
+###############################################################Install additional Windows networking components######
+Install-WindowsFeature RemoteAccess
+Install-WindowsFeature RSAT-RemoteAccess-PowerShell
+Install-WindowsFeature Routing
+Restart-Computer -Force
+
+Install-RemoteAccess -VpnType RoutingOnly
+Set-Service -Name RemoteAccess -StartupType 'Automatic'
+start-service RemoteAccess
+
+###############################################################Install Calico####################################################
+mkdir c:\k
+#Copy the Kubernetes kubeconfig file from the master node (default, Location $HOME/.kube/config), to c:\k\config.
+
+Invoke-WebRequest https://docs.projectcalico.org/scripts/install-calico-windows.ps1 -OutFile c:\install-calico-windows.ps1
+
+c:\install-calico-windows.ps1 -KubeVersion 1.20.0
+
+#Verify that the Calico services are running.
+Get-Service -Name CalicoNode
+Get-Service -Name CalicoFelix
+
+
+#Install and start kubelet/kube-proxy service. Execute following PowerShell script/commands.
+C:\CalicoWindows\kubernetes\install-kube-services.ps1
+Start-Service -Name kubelet
+Start-Service -Name kube-proxy
+
+#Copy kubectl.exe, kubeadm.etc to the folder below which is on the path: 
+cp C:\k\*.exe C:\Users\Administrator\AppData\Local\Microsoft\WindowsApps
+
+
+###############################################################Test Win node#####################################
+#List all cluster nodes
+kubectl get nodes -o wide	
+
+#View the BGP cluster
+sudo calicoctl node status
+
+#Deploy a sample container 
+ kubectl apply -f .\webapi-service.yaml
+
+#Check the pod
+kubectl get pods -o wide 
+
+#Verify the service
+kubectl get services
+
+#Hit the service through its VIP (ClusterIP)
+curl http://<ClusterIP>:8000/system -UseBasicParsing
+
+#His the service throug NodePort
+curl http://<NODE_IP_ADDRESS>:<NODE_PORT_PORT#>/system -UseBasicParsing
+
+#Sh into a POD and call the service
+kubectl exec -it hello-world-5457b44555-cqfct  -- s
+	#Call the service using the ClusterIP
+	curl http://<ClusterIP>:8000/system
+	exit
+
+
+#Cleanup
+kubectl delete deployment webapi-service
+kubectl delete service webapi-service
+
---
+++ setupcluster.sh
@@ -0,0 +1,120 @@
+ssh gary@10.0.0.154
+
+##################### Run this on all Linux nodes #######################
+
+#Update the server
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#Install containerd
+sudo apt-get install containerd -y
+
+#Configure containerd and start the service
+sudo mkdir -p /etc/containerd
+sudo su -
+containerd config default  /etc/containerd/config.toml
+exit
+
+#Next, install Kubernetes. First you need to add the repository's GPG key with the command:
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+
+#Add the Kubernetes repository
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+#Install all of the necessary Kubernetes components with the command:
+sudo apt-get install kubeadm kubelet kubectl -y
+
+#Modify "sysctl.conf" to allow Linux Node’s iptables to correctly see bridged traffic
+sudo nano /etc/sysctl.conf
+    #Add this line: net.bridge.bridge-nf-call-iptables = 1
+
+sudo -s
+#Allow packets arriving at the node's network interface to be forwaded to pods. 
+sudo echo '1' > /proc/sys/net/ipv4/ip_forward
+exit
+
+#Reload the configurations with the command:
+sudo sysctl --system
+
+#Load overlay and netfilter modules 
+sudo modprobe overlay
+sudo modprobe br_netfilter
+  
+#Disable swap by opening the fstab file for editing 
+sudo nano /etc/fstab
+    #Comment out "/swap.img"
+
+#Disable swap from comand line also 
+sudo swapoff -a
+
+#Pull the necessary containers with the command:
+sudo kubeadm config images pull
+
+####### This section must be run only on the Master node##############################################
+sudo kubeadm init 
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Download Calico CNI
+curl https://docs.projectcalico.org/manifests/calico.yaml > calico.yaml
+#Apply Calico CNI
+kubectl apply -f ./calico.yaml
+
+#Copy the "/.kube" folder to other Linux and Windows nodes
+#scp -r $HOME/.kube gary@10.0.0.153:/home/gary
+scp -r $HOME/.kube administrator@10.0.0.194:/users/administrator
+
+#Create folder "c:/k" on Windows node
+ssh administrator@10.0.0.194 'mkdir c:\k'
+#Copy "config" file from master to Windows node
+scp -r $HOME/.kube/config administrator@10.0.0.194:/k/
+
+##################### Run this on other Linux nodes #######################
+exit
+
+ssh YOUR_ID@LINUX_NODE_IP
+    
+sudo -i 
+    #Copy the token and cert from "kubeadm init" operation and run it below
+
+    #Note to join future nodes after inial cluster set up, run "kubeadm token create --print-join-command" to get a new "kubeadm join" with fresh certs.
+exit
+
+exit
+
+##############################################Calico settings (must do you have Windows worker nodes)##################
+#On Linux master:
+	
+	#1.1- Install "calicococtl" on one or more nodes: "https://docs.projectcalico.org/getting-started/clis/calicoctl/install"
+        sudo -i
+        cd /usr/local/bin/
+        curl -o calicoctl -O -L  "https://github.com/projectcalico/calicoctl/releases/download/v3.19.1/calicoctl" 
+        chmod +x calicoctl
+        exit        
+    
+    #1.2- Disable "IPinIP":  
+        calicoctl get ipPool default-ipv4-ippool  -o yaml > ippool.yaml
+        nano ippool.yaml
+        calicoctl apply -f ippool.yaml
+    
+        kubectl get felixconfigurations.crd.projectcalico.org default  -o yaml -n kube-system > felixconfig.yaml
+        nano felixconfig.yaml #Set: "ipipEnabled: false"
+        kubectl apply -f felixconfig.yaml
+
+    #3- Configure strict affinity for clusters using Calico networking
+        # For Linux control nodes using Calico networking, strict affinity must be set to true. 
+        # This is required to prevent Linux nodes from borrowing IP addresses from Windows nodes:"
+             calicoctl ipam configure --strictaffinity=true
+    ""
+    sudo reboot 
+
+################################################################################################################################
+############################################################################Verify Cluster#########################################################################
+#Get cluster info
+kubectl cluster-info
+
+#View nodes (one in our case)
+kubectl get nodes -o wide
+
